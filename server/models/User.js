a712d206ed99f72671c3fe62443b5b77996d5e95
Your Namecd <danielshaw111693@gmail.com>
2023-12-07 21:45:51 -0600

    updated code

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,6 @@ const {Schema, model} = mongoose;
 const UserSchema = new Schema({
   username: {type: String, required: true, min: 4, unique: true},
   password: {type: String, required: true},
-  profilePic: {type: String, default: '/default-image.jpg'}
 });
 
 const UserModel = model('User', UserSchema)
